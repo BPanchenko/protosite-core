0a27b6a53cfbede431ca586fd6211ab0db29ac1c
Boris Panchenko <oberon@list.ru>
2023-01-05 03:00:44 +0300

    add config

---
+++ jest.config.mjs
@@ -0,0 +1,17 @@
+export default {
+	testEnvironment: 'jsdom',
+	preset: 'ts-jest/presets/default-esm',
+	transform: {
+		'^.+\\.m?[tj]s?$': ['ts-jest', { useESM: true }],
+	},
+	moduleNameMapper: {
+		'^(\\.{1,2}/.*)\\.(m)?js$': '$1',
+	},
+	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(m)?ts$',
+	coverageDirectory: 'coverage',
+	collectCoverageFrom: [
+		'custom-element/*.ts',
+		'lib/*.ts',
+		'!custom-element/*.d.ts'
+	]
+}

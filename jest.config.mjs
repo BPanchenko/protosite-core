6c58dbce77bd24131152012750b4e22735f4fd08
Boris Panchenko <oberon@list.ru>
2023-01-22 19:56:51 +0300

    fixed test environment

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,20 +1,10 @@
 export default {
 	collectCoverageFrom: ['custom-element/*.ts', 'lib/*.ts', '!custom-element/*.d.ts'],
 	coverageDirectory: './.jest/coverage',
-	globalSetup: './.jest/setup.cjs',
-	globalTeardown: './.jest/teardown.cjs',
-	moduleNameMapper: {
-		'^(\\.{1,2}/.*)\\.(m)?js$': '$1'
-	},
 	preset: 'jest-puppeteer',
-	testEnvironment: './.jest/puppeteer_environment.cjs',
+	testEnvironment: 'jsdom',
 	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
 	transform: {
-		'^.+\\.ts$': [
-			'ts-jest',
-			{
-				useESM: true
-			}
-		]
+		'^.+\\.ts$': 'ts-jest'
 	}
 }

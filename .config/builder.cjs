58a14e8b3188b9cf7ba74442ad5bfb658c2bc68e
Boris Panchenko <oberon@list.ru>
2023-03-05 10:55:04 +0300

    passing parameteres `type`, `source` and `mode` to builder as env variables

---
+++ .config/builder.cjs
@@ -0,0 +1,80 @@
+const { isString } = require('lodash');
+const path = require('path');
+const glob = require('glob');
+const TerserPlugin = require('terser-webpack-plugin');
+const logger = require('node-color-log');
+const { ROOT, types, getFileEntryByRelativePath } = require('./helpers.cjs');
+
+const OUTPUT = path.resolve(ROOT, `esm`);
+
+module.exports = (env) => {
+  console.dir(env);
+
+  if (isString(env.type) && types.selectByProperty('dashed', env.type) === false) {
+    logger.error(
+      `Parameter "env.type" can have only one of thees values: "${types.CE.dashed}", "${types.WC.dashed}"`
+    );
+    process.exit(1);
+  }
+
+  const source = env.source || types.selected.source;
+  const suffix = types.selected ? types.selected.dashed : '';
+
+  const ENTRIES = Object.fromEntries(
+    glob
+      .sync(`src/${source}/*.ts`, {
+        dot: true,
+        ignore: ['**/*.d.ts', '**/__*__/**']
+      })
+      .map((relative) => getFileEntryByRelativePath(relative, suffix))
+  );
+
+  return {
+    mode: env.mode || 'development',
+    entry: ENTRIES,
+    target: 'web',
+    devtool: false,
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          loader: 'ts-loader',
+          options: {
+            configFile: 'tsconfig.build.json'
+          }
+        },
+        {
+          test: /\.css$/,
+          loader: 'css-loader',
+          options: {
+            exportType: 'css-style-sheet',
+            modules: {
+              mode: 'global',
+              exportGlobals: true
+            }
+          }
+        }
+      ]
+    },
+    resolve: {
+      alias: {
+        '@uikit': path.resolve(ROOT, 'node_modules/@bpanchenko/uikit/assets')
+      },
+      extensions: ['.ts']
+    },
+    output: {
+      filename: '[name].mjs',
+      path: OUTPUT
+    },
+    optimization: {
+      minimize: false,
+      minimizer: [
+        new TerserPlugin({
+          minify: TerserPlugin.uglifyJsMinify,
+          terserOptions: {}
+        })
+      ],
+      usedExports: true
+    }
+  };
+};
---
+++ .config/helpers.cjs
@@ -0,0 +1,71 @@
+const path = require('path');
+const { kebabCase, trim } = require('lodash');
+
+const ROOT = process.cwd();
+
+const getFileEntryByRelativePath = (relativePath, typeName = '') => {
+  const absolutePath = path.resolve(ROOT, relativePath);
+  const props = path.parse(absolutePath);
+
+  let name = kebabCase(props.name + '-' + (typeName || ''));
+  name = trim(name, '-');
+
+  return [name, absolutePath];
+};
+
+const types = Object.create(
+  {
+    get selected() {
+      const proto = Object.getPrototypeOf(this);
+      const key = proto.selectedKey;
+      return key ? this[key] : null;
+    },
+    selectedKey: false,
+    selectByProperty: (name, value) => {
+      const proto = Object.getPrototypeOf(types);
+      const keys = Object.keys(types);
+      for (const key of keys) {
+        const data = types[key];
+        const selected = data.hasOwnProperty(name) && data[name] === value;
+        proto.selectedKey = selected ? key : false;
+        if (selected) break;
+      }
+      return proto.selectedKey;
+    }
+  },
+  {
+    CE: {
+      value: Object.freeze({
+        name: 'CustomElement',
+        dashed: 'custom-element',
+        plural: 'custom-elements',
+        source: 'component'
+      }),
+      enumerable: true
+    },
+    CP: {
+      value: Object.freeze({
+        name: 'CorePart',
+        dashed: 'core-part',
+        plural: 'core-parts',
+        source: 'trunk'
+      }),
+      enumerable: true
+    },
+    WC: {
+      value: Object.freeze({
+        name: 'WebComponent',
+        dashed: 'web-component',
+        plural: 'web-components',
+        source: 'component'
+      }),
+      enumerable: true
+    }
+  }
+);
+
+module.exports = {
+  ROOT,
+  getFileEntryByRelativePath,
+  types
+};

aac061f0bbce851e842dd9a6efb8b1879f659ff4
Boris Panchenko <oberon@list.ru>
2023-03-11 16:37:52 +0300

    separation the code of e2e testing

--- src/component/__specs__/avatar.spec.ts
+++ src/component/__specs__/avatar.spec.ts
@@ -1,52 +1,15 @@
-import AvatarElement from '../avatar';
-import { configureToMatchImageSnapshot } from 'jest-image-snapshot';
+import AvatarElement, { AvatarMetadata } from '../avatar';
 
-expect.extend({
-	toMatchImageSnapshot: configureToMatchImageSnapshot({
-		comparisonMethod: 'ssim',
-		failureThreshold: 0.1,
-		failureThresholdType: 'percent'
-	})
-});
-jest.setTimeout(60000);
+console.log(AvatarMetadata);
+console.log(AvatarElement);
 
 describe('AvatarElement', () => {
-	const avatar = new AvatarElement();
+	const avatar = document.createElement('abbr');
 
-	it('Markup', () => {
+	console.log(avatar);
+
+	it('markup', () => {
 		document.body.appendChild(avatar);
 		expect(document.body).toMatchSnapshot();
-	});
-
-	describe('Visual Regression', () => {
-		let containerHandle, image;
-
-		beforeAll(async () => {
-			const page = await global.browser.newBlankPage();
-
-			containerHandle = await page.$('#container');
-			await page.evaluate((container) => {
-				const avatar = document.createElement('c-avatar');
-				container.appendChild(avatar);
-			}, containerHandle);
-
-			image = await page.screenshot({ fullPage: true });
-		});
-
-		afterAll(async () => {
-			await containerHandle.dispose();
-		});
-
-		it('horizontal', async () => {
-			expect(image).toMatchImageSnapshot({
-				diffDirection: 'horizontal'
-			});
-		});
-
-		it('vertical', async () => {
-			expect(image).toMatchImageSnapshot({
-				diffDirection: 'vertical'
-			});
-		});
 	});
 });

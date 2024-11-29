7ed95d6fe7f1fd3d7a73397583fa464f7cb79e36
Boris Vasilievich Panchenko <oberon@list.ru>
2024-11-29 08:52:21 +0300

    Init: User Interaction

--- src/components/select-field.js
+++ src/components/select-field.js
@@ -16,6 +16,9 @@ class SelectField extends HTMLElement {
 	/** @type {ElementInternals} */
 	#internals_
 
+	/** @type {boolean} */
+	#interactive = false
+
 	static formAssociated = false
 
 	/** @type Array<SelectField.Attributes> */
@@ -56,74 +59,13 @@ class SelectField extends HTMLElement {
 
 	connectedCallback() {
 		this.$button.addEventListener('click', () => this.toggle())
+		this.addEventListener('focus', this.#onFocus)
+		this.addEventListener('blur', this.#onBlur)
 	}
 
 	selectOption(idx) {
 		this.options[idx].ariaSelected = true
 		this.options[idx].ariaChecked = true
-	}
-
-	$byID(elId) {
-		return this.#shadow_.getElementById(elId)
-	}
-
-	get $button() {
-		return this.#shadow_.querySelector('[role=button]')
-	}
-
-	/**
-	 * @type {HTMLInputElement}
-	 *
-	 * Resizes the component. The default size is defined in UIKit.
-	 *
-	 * [MDN Reference](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Functions/get)
-	 */
-	get $input() {
-		return this.#shadow_.querySelector('input')
-	}
-
-	get $listbox() {
-		return this.#shadow_.querySelector('[role=listbox]')
-	}
-
-	get $options() {
-		return this.#shadow_.querySelectorAll('[role=option]')
-	}
-
-	get $status() {
-		return this.#shadow_.querySelector('[role=status]')
-	}
-
-	/** @param {Array<string>} selector */
-	/*
-
-	$options(selector) {
-		return this.#shadow_.querySelectorAll('[role=option]')
-	}
-	*/
-
-	get value() {
-		const result = this.$input.value
-		this.setAttribute('value', result)
-		return result
-	}
-
-	#applyAttributes(attrs = {}) {
-		const valid = SelectField.observedAttributes
-		const pairs = Object.entries(attrs)
-		const badly = pairs.filter(([attr]) => valid.includes(attr) === false)
-		const goodly = pairs.filter(([attr]) => valid.includes(attr))
-
-		if (badly.length > 0)
-			console.warn(
-				`Unsupported attributes: "${badly.map(([key]) => key).join(', ')}"`,
-			)
-
-		goodly.forEach(([key, value]) => this.setAttribute(key, value))
-	}
-
-	set exportparts(value) {
-		throw new Error(`Don't Change! ${value.toString()}`)
 	}
 
 	/**
@@ -141,28 +83,159 @@ class SelectField extends HTMLElement {
 		} else {
 			states.add('collapsed')
 			states.delete('expanded')
+			this.#focus(this.$button)
 		}
-
-		this.$button.ariaExpanded = expanded
+		this.ariaExpanded = expanded
 		return expanded ? 'expanded' : 'collapsed'
 	}
 
-	static onFocus(ctx, event) {
-		console.log('[EVENT]:', event, ctx)
-	}
-	static OnClick(ctx, event) {
-		console.log('[EVENT]:', event, ctx)
+	get focused() {
+		return this.#focus()
 	}
 
-	static onKeyUp(ctx, event) {
-		console.log('[EVENT]:', event, ctx)
+	get focusedRole() {
+		return this.focused.getAttribute('role')
+	}
+
+	get interactive() {
+		return this.#interactive
+	}
+
+	get value() {
+		const result = this.$input.value
+		this.setAttribute('value', result)
+		return result
+	}
+
+	set exportparts(value) {
+		throw new Error(`Don't Change! ${value.toString()}`)
+	}
+
+	/** @type {HTMLDivElement} */
+	get $button() {
+		return this.#shadow_.querySelector('[role=button]')
+	}
+
+	/** @type {HTMLInputElement} */
+	get $input() {
+		return this.#shadow_.querySelector('input')
+	}
+
+	/** @type {HTMLDivElement} */
+	get $status() {
+		return this.#shadow_.querySelector('[role=status]')
+	}
+
+	/** @type {HTMLDivElement} */
+	get $listbox() {
+		return this.#shadow_.querySelector('[role=listbox]')
+	}
+
+	/** @type {NodeListOf<HTMLElement>} */
+	get $options() {
+		return this.$listbox.querySelectorAll('[role=option]')
+	}
+
+	/** @type {HTMLElement} */
+	get $lastOption() {
+		return this.$listbox.querySelectorAll('[role=option]:last-of-type')
+	}
+
+	/** @type {HTMLElement} */
+	get $firstOption() {
+		return this.$listbox.querySelectorAll('[role=option]:first-of-type')
+	}
+
+	#applyAttributes(attrs = {}) {
+		const valid = SelectField.observedAttributes
+		const pairs = Object.entries(attrs)
+		const badly = pairs.filter(([attr]) => valid.includes(attr) === false)
+		const goodly = pairs.filter(([attr]) => valid.includes(attr))
+
+		if (badly.length > 0)
+			console.warn(
+				`Unsupported attributes: "${badly.map(([key]) => key).join(', ')}"`,
+			)
+
+		goodly.forEach(([key, value]) => this.setAttribute(key, value))
+	}
+
+	/**
+	 * Set/Get focused element
+	 *
+	 * @param {HTMLElement} [element]
+	 * @returns {HTMLElement} Current focused element
+	 */
+	#focus(element) {
+		let focused = this.#focus()
+		if (HTMLElement.prototype.isPrototypeOf(element)) {
+			if (focused !== null)
+				focused.removeAttribute('aria-activedescendant')
+			element.setAttribute('aria-activedescendant', 'true')
+			focused = element
+		}
+		if (focused) return focused
+		else return this.#focus(this.$button)
+	}
+
+	#setInteractionHandlers() {
+		if (this.#interactive === false) {
+			this.addEventListener('click', this.#onClick)
+			this.addEventListener('keypress', this.#onKeyPress)
+			this.#interactive = true
+		}
+	}
+
+	#deleteInteractionHandlers() {
+		if (this.#interactive) {
+			this.removeEventListener('click', this.#onClick)
+			this.removeEventListener('keypress', this.#onKeyPress)
+			this.#interactive = false
+		}
+	}
+
+	#onFocus() {
+		this.#setInteractionHandlers()
+	}
+
+	#onClick() {}
+
+	#onKeyPress({ key }) {
+		switch (key) {
+			case 'Backspace':
+			case 'Enter':
+				if (this.focused === this.$button) {
+					this.toggle('expanded')
+				}
+				break
+			case 'Escape':
+				if (this.focused !== this.$button) {
+					this.toggle('collapsed')
+				}
+				break
+			case 'End':
+				this.focus(this.$lastOption)
+				break
+			case 'Home':
+				this.focus(this.$firstOption)
+				break
+			case 'ArrowUp':
+				break
+			case 'ArrowDown':
+				break
+			default:
+				if (/\w+/.test(key)) {
+					// TODO
+				}
+		}
+	}
+
+	#onBlur() {
+		this.#deleteInteractionHandlers()
+		this.toggle('collapsed')
 	}
 }
 
 customElements.define(tagName, SelectField)
 
 export default customElements.get(tagName)
-
-/**
- *
- */

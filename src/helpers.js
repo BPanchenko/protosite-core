9ec847eb203013b95eeee269f9c262db31fe2b00
Boris Vasilievich Panchenko <oberon@list.ru>
2024-12-02 21:03:07 +0300

    Init: Web Component Initialization Helpers

--- src/helpers.js
+++ src/helpers.js
@@ -7,6 +7,56 @@ Object.defineProperty(String.prototype, 'capitalize', {
 	enumerable: false,
 })
 
+/**
+ * @type {($element: HTMLElement, hash: Record<string, string>) => void}
+ *
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Element/setAttribute)
+ */
+export const initAttributes = ($element, hash) =>
+	Object.entries(hash).forEach(([key, value]) =>
+		$element.setAttribute(key, value),
+	)
+
+/**
+ * The function attaches a shadow DOM tree to the specified element and returns a reference to its ShadowRoot.
+ *
+ * @param {HTMLElement} $element
+ * @param {Object} options
+ * @param {HTMLTemplateElement} options.$template
+ * @param {boolean} options.delegatesFocus - If true, when a non-focusable part of the shadow DOM is clicked, or .focus() is called on the host element, the first focusable part is given focus, and the shadow host is given any available :focus styling.
+ * @param {"closed" | "open"} options.mode - When the mode of a shadow root is "closed", the shadowroot implementation internals are inaccessible and unchangeable.
+ * @param {boolean} options.serializable - If set, the shadow root may be serialized by calling the Element.getHTML() or ShadowRoot.getHTML() methods with the options.serializableShadowRoots parameter set true.
+ * @param {"manual" | "named"} options.slotAssignment
+ * @returns {ShadowRoot} ShadowRoot of the specified element
+ *
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Element/attachShadow)
+ *
+ * [About `ShadowRoot.delegatesFocus` property](https://developer.mozilla.org/en-US/docs/Web/API/ShadowRoot/delegatesFocus)
+ *
+ * [About `ShadowRoot.mode` property](https://developer.mozilla.org/en-US/docs/Web/API/ShadowRoot/mode)
+ *
+ * [About `ShadowRoot.serializable` property](https://developer.mozilla.org/en-US/docs/Web/API/ShadowRoot/serializable)
+ *
+ * [About `ShadowRoot.slotAssignment` property](https://developer.mozilla.org/en-US/docs/Web/API/ShadowRoot/slotAssignment)
+ */
+export const initShadowRoot = ($element, options) => {
+	const {
+		$template,
+		delegatesFocus = false,
+		mode = 'closed',
+		serializable = false,
+	} = options
+
+	const node = $element.attachShadow({
+		delegatesFocus,
+		mode,
+		serializable,
+	})
+
+	node.appendChild($template.cloneNode(true))
+	return node
+}
+
 export const checkFontFace = (search) =>
 	document.fonts
 		.values()

f3b624c6770f192c647ac2e8c1386d2c2db42450
Boris Vasilievich Panchenko <oberon@list.ru>
2024-12-07 21:16:49 +0300

    Tests: Functional Specification of the select field

--- src/component/__specs__/select-field.spec.js
+++ src/component/__specs__/select-field.spec.js
@@ -1,11 +1,45 @@
-//import '../__mocks__/window.mock'
+import '../__mocks__/@github.template-parts.mock'
+import '../__mocks__/window.mock'
 import SelectComponent from '../select-field.js'
 
+import pug from 'pug'
+import fs from 'node:fs'
+import path from 'node:path'
+
+const sourcePugCode = fs.readFileSync(
+	path.resolve('src/component/select-field.pug'),
+	{
+		encoding: 'utf-8',
+	},
+)
+const sourceHtmlCode = pug.compile(sourcePugCode)()
+
 describe('[SelectComponent]', () => {
-	describe('State:', () => {
-		const select = new SelectComponent()
+	describe('Init:', () => {
+		const select = new SelectComponent({
+			name: 'custom-field-fruit',
+			'aria-placeholder': 'Выберите фрукт...',
+			label: 'Выбранный фрукт:',
+		})
 		document.body.appendChild(select)
 
 		expect(select.state).toMatchInlineSnapshot()
+		expect(select).toMatchInlineSnapshot()
+	})
+
+	describe('Interactive:', () => {
+		it('Click Left Button', () => {})
+
+		it('Click Right Button', () => {})
+
+		it('Press [Enter]', () => {})
+
+		it('Press [Escape]', () => {})
+
+		it('Press Backspace', () => {})
+
+		it('Press End', () => {})
+
+		it('Press Home', () => {})
 	})
 })

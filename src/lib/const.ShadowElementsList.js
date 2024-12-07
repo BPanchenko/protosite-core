8bf4f53116662ebdffb218f2c5e828fe6f7e484e
Boris Vasilievich Panchenko <oberon@list.ru>
2024-12-07 22:12:13 +0300

    Init: Elements you can attach a shadow to

---
+++ src/lib/const.ShadowElementsList.js
@@ -0,0 +1,22 @@
+const ShadowElementsList = [
+	'article',
+	'aside',
+	'blockquote',
+	'body',
+	'div',
+	'footer',
+	'h1',
+	'h2',
+	'h3',
+	'h4',
+	'h5',
+	'h6',
+	'header',
+	'main',
+	'nav',
+	'p',
+	'section',
+	'span',
+]
+
+export default ShadowElementsList

5508fdad1ad2213e49f36a4fc7a8fc52aa46aa77
Boris Vasilievich Panchenko <oberon@list.ru>
2025-03-03 20:57:29 +0300

    Add: Builder based on the esbuild bundler

---
+++ .kernel/builder.js
@@ -0,0 +1,41 @@
+import { dtsPlugin } from 'esbuild-plugin-d.ts'
+import { writeFileSync } from 'node:fs'
+
+import pugCompiler from './compiler.pug.js'
+import { analyzeMetafileSync, build } from 'esbuild'
+
+const bundle = await build({
+	bundle: true,
+	entryNames: '[name]',
+	entryPoints: [
+		{ out: 'component.avatar', in: 'source/component/Avatar/index.ts' },
+		{ out: 'component.select', in: 'source/component/Select/index.ts' },
+		{ out: 'element.arrow', in: 'source/element/Arrow/index.ts' },
+		{ out: 'element.listbox', in: 'source/element/Listbox/index.ts' },
+		{ out: 'element.option', in: 'source/element/Option.ts' },
+	],
+	mainFields: ['browser', 'main', 'module'],
+	metafile: true,
+	minify: true,
+	outdir: 'assets',
+	outExtension: {
+		'.js': '.mjs',
+	},
+	outbase: 'source',
+	format: 'esm',
+	sourcemap: false,
+	target: ['esnext', 'safari18'],
+	tsconfig: 'tsconfig.json',
+	plugins: [pugCompiler(), dtsPlugin()],
+})
+
+writeFileSync(
+	'assets/bundle-analysis.json',
+	JSON.stringify(bundle.metafile, undefined, 2),
+)
+
+console.log(
+	analyzeMetafileSync(bundle.metafile, {
+		verbose: true,
+	}),
+)

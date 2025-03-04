9216c6bf24e8e6dc21bc4de56114607cce659ea5
Boris Vasilievich Panchenko <oberon@list.ru>
2025-03-04 07:29:59 +0300

    Upd: Assets of the updated package builder

--- assets/index.d.ts
+++
@@ -1,6 +0,0 @@
-export type * from './component.avatar.d.mts'
-export type * from './component.select.d.mts'
-export type * from './element.arrow.d.mts'
-export type * from './element.listbox.d.mts'
-export type * from './element.option.d.mts'
-export type * from './element.option.d.mts'

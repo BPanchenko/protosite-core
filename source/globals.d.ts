c48d7a83cb9aee228892bb23294b2aa7ccb493a8
Boris Vasilievich Panchenko <oberon@list.ru>
2025-05-05 06:23:48 +0300

    Bugfix: Extending the "CustomStateSet" and "CSSRuleList" interfaces for features available since may 2024

--- source/globals.d.ts
+++ source/globals.d.ts
@@ -1,3 +1,13 @@
+interface CustomStateSet {
+	has(state: string): boolean
+	add(state: string): void
+	delete(state: string): void
+}
+
+interface CSSRuleList {
+	[Symbol.iterator](): Iterator<CSSStyleRule>
+}
+
 declare module '*.pug' {
 	const template: string
 	export default template
